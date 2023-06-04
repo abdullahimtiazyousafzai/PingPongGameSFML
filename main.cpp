20577cf643919b5f79289f37f651d3989002a917
Muhammad Abdullah <126229662+Abdullah2022323@users.noreply.github.com>
2023-06-04 12:58:58 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,6 +18,40 @@ int main()
     if (!hitbuffer.loadFromFile("F:/CLionProjects/PingPongGameSFML/hit.wav"))
         return -1;
 
+    sf::RectangleShape background(sf::Vector2f(window_width, window_height));
+    background.setFillColor(sf::Color(0,100,0));
+
+    // Create the border lines
+    sf::RectangleShape topLine(sf::Vector2f(window_width - 60, 2));
+    topLine.setPosition(30, 20);
+    topLine.setFillColor(sf::Color::Black);
+
+    sf::RectangleShape bottomLine(sf::Vector2f(window_width - 60, 2));
+    bottomLine.setPosition(30, 700);
+    bottomLine.setFillColor(sf::Color::Black);
+
+    sf::RectangleShape leftLine(sf::Vector2f(2, window_height - 40));
+    leftLine.setPosition(30, 20);
+    leftLine.setFillColor(sf::Color::Black);
+
+    sf::RectangleShape rightLine(sf::Vector2f(2, window_height - 40));
+    rightLine.setPosition(1150, 20);
+    rightLine.setFillColor(sf::Color::Black);
+
+    sf::RectangleShape centerline;
+    centerline.setSize(sf::Vector2f(2, window_height - 40));
+    centerline.setPosition(((window_width - 2) / 2) + 4, 20);
+    centerline.setFillColor(sf::Color::Black);
+
+    sf::CircleShape centercircle;
+    centercircle.setRadius(100);
+    centercircle.setPosition(window_width /2 -95, window_height / 2 -98 );
+    centercircle.setOutlineThickness(2);
+    centercircle.setOutlineColor(sf::Color::Black);
+    centercircle.setFillColor(sf::Color::Transparent);
+
+    
+
     sf::Sound hitSound;
     hitSound.setBuffer(hitbuffer);
 
@@ -29,7 +63,7 @@ int main()
     missSound.setBuffer(missbuffer);
 
     sf::Font font;
-    if (!font.loadFromFile("F:/CLionProjects/SnakeGameSFML/Roboto-Light.ttf"))
+    if (!font.loadFromFile("F:/CLionProjects/SnakeGameSFML/Lindelof-Font.ttf"))
     {
         std::cout << "Error loading font" << std::endl;
         return -1;
@@ -39,8 +73,8 @@ int main()
     pressSpace.setFont(font);
     pressSpace.setString("Press Space to Play the game!");
     pressSpace.setCharacterSize(30);
-    pressSpace.setPosition(400.f, 300.0f);
-    pressSpace.setFillColor(sf::Color::White);
+    pressSpace.setPosition(400.f, 15.0f);
+    pressSpace.setFillColor(sf::Color::Red);
 
     bool gameStarted = false;
 
@@ -52,19 +86,23 @@ int main()
     sf::RectangleShape paddle_left(sf::Vector2f(paddle_width, paddle_height));
     sf::RectangleShape paddle_right(sf::Vector2f(paddle_width, paddle_height));
     paddle_left.setPosition(0.0f, (window_height - paddle_height) / 2.0f);
-    paddle_left.setFillColor(sf::Color::Red);
+    paddle_left.setFillColor(sf::Color::Black);
+    paddle_left.setOutlineThickness(4);
+    paddle_left.setOutlineColor(sf::Color::Blue);
     paddle_right.setPosition(window_width - paddle_width, (window_height - paddle_height) / 2.0f);
-    paddle_right.setFillColor(sf::Color::Blue);
+    paddle_right.setFillColor(sf::Color::Black);
+    paddle_right.setOutlineThickness(4);
+    paddle_right.setOutlineColor(sf::Color::Red);
 
 
 
     // Set up the ball
     float ball_size = 10.0f;
     sf::CircleShape ball(ball_size);
-    ball.setPosition((window_width - ball_size) / 2.0f, (window_height - ball_size) / 2.0f);
+    ball.setPosition((window_width - ball_size) / 2, (window_height - ball_size) / 2);
     float ball_speed_x = 5.f;
     float ball_speed_y = 5.f;
-    ball.setFillColor(sf::Color::Yellow);
+    ball.setFillColor(sf::Color::Cyan);
 
     // Set up the score
     int score_left = 0;
@@ -72,12 +110,14 @@ int main()
 
     sf::Text score_text_left;
     score_text_left.setFont(font);
+    score_text_left.setFillColor(sf::Color::Blue);
     score_text_left.setCharacterSize(30);
-    score_text_left.setPosition(10.f, 10.0f);
+    score_text_left.setPosition(200.f, 20.0f);
     sf::Text score_text_right;
     score_text_right.setFont(font);
+    score_text_right.setFillColor(sf::Color::Red);
     score_text_right.setCharacterSize(30);
-    score_text_right.setPosition(850.f , 10.0f);
+    score_text_right.setPosition(window_width - 370 , 20.0f);
 
     sf::Text pressEsc;
 
@@ -90,7 +130,7 @@ int main()
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
             {
                 window.close();
             }
@@ -135,12 +175,15 @@ int main()
             // Check if the ball collides with the paddles and change its horizontal direction if it does
             if (ball.getGlobalBounds().intersects(paddle_left.getGlobalBounds()))
             {
+                if(ball_speed_x >= -10.f)
                 ball_speed_x -= 0.8f;
+
                 hitSound.play();
                 ball_speed_x = -ball_speed_x;
             }
             if(ball.getGlobalBounds().intersects(paddle_right.getGlobalBounds()))
             {
+                if(ball_speed_x <= 10.f)
                 ball_speed_x += 0.8f;
                 hitSound.play();
                 ball_speed_x = -ball_speed_x;
@@ -148,18 +191,18 @@ int main()
 
             // Check if the ball goes out of bounds on the left or right and update the score
             if (ball.getPosition().x < 0.0f) {
-                ball_speed_x = 7.f;
+                ball_speed_x = 5.f;
                 missSound.play();
                 score_right++;
                 ball.setPosition((window_width - ball_size) / 2.0f, (window_height - ball_size) / 2.0f);
                 ball_speed_x = -ball_speed_x;
             }
             if (ball.getPosition().x + ball_size > window_width) {
-                ball_speed_x = 7.f;
+                ball_speed_x = 5.f;
                 missSound.play();
                 score_left++;
                 ball.setPosition((window_width - ball_size) / 2.0f, (window_height - ball_size) / 2.0f);
-                ball_speed_x = -ball_speed_x;
+                ball_speed_x = ball_speed_x;
             }
 
             // Update the score text
@@ -189,7 +232,13 @@ int main()
         }
         // Clear the window
         window.clear();
-
+        window.draw(background);
+        window.draw(topLine);
+        window.draw(bottomLine);
+        window.draw(leftLine);
+        window.draw(rightLine);
+        window.draw(centercircle);
+        window.draw(centerline);
         // Draw the paddles, ball, and score text
         window.draw(paddle_left);
         window.draw(paddle_right);
